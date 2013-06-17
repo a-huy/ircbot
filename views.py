42cf5099415c13bf0f706b987796c7eb9f8e631e
Andy Nguyen <anguyenhuy@gmail.com>
2013-06-16 21:18:28 -0700

    changed debug statements in callback

--- views.py
+++ views.py
@@ -6,9 +6,9 @@ from django.template import RequestContext
 def callback(request):
     method = request.META['REQUEST_METHOD']
     print method
-    print request
     if method == 'GET':
         return HttpResponse('WHATCHU LOOKIN AT?!')
     elif method == 'POST':
         print request.POST
+        print request.raw_post_data
     return HttpResponse(request)
